2fcc47d3df157d32468495ddca28758567b221fe
DHAIRYADHOLU <dhairyadholu@gmail.com>
2024-09-26 08:41:49 -0400

    Added project configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mgray: "#212121",
+        mlightgray: "#2f2f2f",
+      },
+    },
+  },
+  plugins: [],
+};
