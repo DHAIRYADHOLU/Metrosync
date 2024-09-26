276f30d640dbd7a4f52662b7e6531b5fcd0529c7
DHAIRYADHOLU <dhairyadholu@gmail.com>
2024-09-26 19:59:27 -0400

    Created frontend folder

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connection successful!");
+  } catch (error) {
+    console.error("Connection failed:", error.message);
+    process.exit(1); // Stop the server if connection fails
+  }
+};
+
+// Call the connection function
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
